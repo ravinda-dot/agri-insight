cf4bef88be268500d9e0e83b677ec6d07b454d05
ravinda-dot <143981199+ravinda-dot@users.noreply.github.com>
2026-02-08 07:34:36 +0530

    Update PredictionPage.jsx

--- frontend/src/pages/PredictionPage.jsx
+++ frontend/src/pages/PredictionPage.jsx
@@ -3,7 +3,7 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 import axios from 'axios';
 
 const API_BASE_URL = 'http://127.0.0.1:8000';
-const GEMINI_API_KEY = "AIzaSyDoUaAB-2fk0pH3qIPrAysUaRO8qBjuktg";
+const GEMINI_API_KEY = "api key.....";
 const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${GEMINI_API_KEY}`;
 
 
